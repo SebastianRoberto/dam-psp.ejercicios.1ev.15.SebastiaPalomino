46aa66eda37b1000e1b8434e2283cb157a967e66
Sebastian <sebastian.roberto.pp@gmail.com>
2024-10-29 20:04:28 +0100

    SES05-06-EJ06-AP04

--- sesion-05-06/EJ06/main.go
+++ sesion-05-06/EJ06/main.go
@@ -7,40 +7,44 @@ import (
     "strings"
 )
 
-func contarLetra(nombreArchivo string, letra string, ch chan<- int) {
+func contarLetraA(nombreArchivo string, ch chan<- int) {
     contenido, err := ioutil.ReadFile(nombreArchivo)
     if err != nil {
-        ch <- 0
+        ch <- -1 // Usamos -1 para indicar un error
         return
     }
-    texto := string(contenido)
-    cuenta := strings.Count(texto, letra) + strings.Count(texto, strings.ToUpper(letra))
+    texto := strings.ToLower(string(contenido))
+    cuenta := strings.Count(texto, "a")
     ch <- cuenta
 }
 
 func main() {
-    if len(os.Args) < 3 {
-        fmt.Println("Uso: go run main.go <letra> <nombreArchivo1> <nombreArchivo2> ...")
+    if len(os.Args) < 2 {
+        fmt.Println("Uso: go run main.go <nombreArchivo1> <nombreArchivo2> ...")
         return
     }
 
-    letra := os.Args[1]
     ch := make(chan int)
     total := 0
 
-    for _, nombreArchivo := range os.Args[2:] {
-        go contarLetra(nombreArchivo, letra, ch)
+    for _, nombreArchivo := range os.Args[1:] {
+        go contarLetraA(nombreArchivo, ch)
     }
 
-    for range os.Args[2:] {
+    for i, nombreArchivo := range os.Args[1:] {
         cuenta := <-ch
-        total += cuenta
-        if cuenta == 0 {
+        if cuenta == -1 {
             fmt.Printf("Error al abrir el archivo: %s\n", nombreArchivo)
         } else {
-            fmt.Printf("La letra '%s' aparece %d veces en el archivo %s.\n", letra, cuenta, nombreArchivo)
+            total += cuenta
+            fmt.Printf("La letra 'a' aparece %d veces en el archivo %s.\n", cuenta, nombreArchivo)
+        }
+        
+        if i == len(os.Args[1:])-1 {
+            close(ch)
         }
     }
 
-    fmt.Printf("Total de apariciones de la letra '%s': %d\n", letra, total)
+    fmt.Printf("Total de apariciones de la letra 'a': %d\n", total)
 }
+
