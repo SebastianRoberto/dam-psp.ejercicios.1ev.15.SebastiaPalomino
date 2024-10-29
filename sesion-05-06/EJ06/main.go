f30fed8ed2dde70711b6d000a742862e25a44cad
Sebastian <sebastian.roberto.pp@gmail.com>
2024-10-29 19:23:27 +0100

    SES05-06-EJ06-AP01

--- sesion-05-06/EJ06/main.go
+++ sesion-05-06/EJ06/main.go
@@ -8,33 +8,33 @@ import (
 )
 
 
-func contarLetraA(nombreArchivo string) (int, error) {
+func contarLetraA(nombreArchivo string, ch chan int) {
     contenido, err := ioutil.ReadFile(nombreArchivo)
     if err != nil {
-        return 0, err 
+        fmt.Printf("Error al abrir el archivo: %v\n", err)
+        ch <- 0
+        return
     }
 
     texto := string(contenido)
-    
     cuenta := strings.Count(texto, "a") + strings.Count(texto, "A")
-    return cuenta, nil
+    ch <- cuenta 
 }
 
 func main() {
-    
     if len(os.Args) < 2 {
         fmt.Println("Uso: go run main.go <nombreArchivo>")
         return
     }
 
     nombreArchivo := os.Args[1]
+    ch := make(chan int)
 
     
-    cuenta, err := contarLetraA(nombreArchivo)
-    if err != nil {
-        fmt.Printf("Error al abrir el archivo: %v\n", err)
-        return
-    }
+    go contarLetraA(nombreArchivo, ch)
 
-    fmt.Printf("La letra 'a' aparece %d veces en el archivo %s\n", cuenta, nombreArchivo)
+    
+    cuenta := <-ch
+    fmt.Printf("La letra 'a' aparece %d veces en el archivo %s.\n", cuenta, nombreArchivo)
 }
+
