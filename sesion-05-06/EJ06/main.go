467915d2a0f04e81a007fbc387101e41156ab51c
Sebastian <sebastian.roberto.pp@gmail.com>
2024-10-29 20:32:26 +0100

    SES05-06-EJ06-AP05

--- sesion-05-06/EJ06/main.go
+++ sesion-05-06/EJ06/main.go
@@ -7,44 +7,53 @@ import (
     "strings"
 )
 
-func contarLetraA(nombreArchivo string, ch chan<- int) {
+func contarLetra(nombreArchivo string, letra string, ch chan<- int) {
     contenido, err := ioutil.ReadFile(nombreArchivo)
     if err != nil {
         ch <- -1 // Usamos -1 para indicar un error
         return
     }
     texto := strings.ToLower(string(contenido))
-    cuenta := strings.Count(texto, "a")
+    letraBuscada := strings.ToLower(letra)
+    cuenta := strings.Count(texto, letraBuscada)
     ch <- cuenta
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Uso: go run main.go <nombreArchivo1> <nombreArchivo2> ...")
+    if len(os.Args) < 3 {
+        fmt.Println("Uso: go run main.go <letra> <nombreArchivo1> <nombreArchivo2> ...")
+        return
+    }
+
+    letra := os.Args[1]
+    if len(letra) != 1 {
+        fmt.Println("Por favor, ingrese una sola letra como primer argumento.")
         return
     }
 
     ch := make(chan int)
     total := 0
 
-    for _, nombreArchivo := range os.Args[1:] {
-        go contarLetraA(nombreArchivo, ch)
+    for _, nombreArchivo := range os.Args[2:] {
+        go contarLetra(nombreArchivo, letra, ch)
     }
 
-    for i, nombreArchivo := range os.Args[1:] {
+    for i, nombreArchivo := range os.Args[2:] {
         cuenta := <-ch
         if cuenta == -1 {
             fmt.Printf("Error al abrir el archivo: %s\n", nombreArchivo)
         } else {
             total += cuenta
-            fmt.Printf("La letra 'a' aparece %d veces en el archivo %s.\n", cuenta, nombreArchivo)
+            fmt.Printf("La letra '%s' aparece %d veces en el archivo %s.\n", letra, cuenta, nombreArchivo)
         }
         
-        if i == len(os.Args[1:])-1 {
+        if i == len(os.Args[2:])-1 {
             close(ch)
         }
     }
 
-    fmt.Printf("Total de apariciones de la letra 'a': %d\n", total)
+    fmt.Printf("Total de apariciones de la letra '%s': %d\n", letra, total)
 }
+
+
 
